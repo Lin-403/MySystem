3b7ee0ad2b05f098bac9959d358451bb795ede65
Lin <1537053400@qq.com>
2022-06-23 18:02:22 +0800

    msg

---
+++ mysystem/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss'
+
+
+export default function App() {
+  return (
+    <div>App
+      <ul>
+        <li>123</li>
+        <li>111</li>
+        <li>22222</li>
+      </ul>
+    </div>
+  );
+}
